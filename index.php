dd93c7c31b1ee958d75cf484bc1f38c0c50705fe
Amalia Litsa <cottonwood@cottonwood.local>
2020-04-23 13:21:46 -0500

    first email done

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>SPM Trail Email Comms</title>
+<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+<base target="_blank">
+<style type="text/css">
+  html, body {font-family: 'open sans', sans-serif; color: #002838; line-height: 1.6em; margin: 2em;}
+  a {color: #7E5CEF; text-decoration: none;}
+  td {padding-right: 1em;}
+</style>
+</head>
+
+<body>
+  <h1>Discover</h1>
+  <p><strong>30 day offer</strong></p>
+  <table>
+    <tbody>
+      <tr><td>T</td><td><a href="disc-aware.html">Become aware of trail</a></td></tr>
+      <tr><td>T + 14</td><td><a href="disc-remind.html">Reminder to check it out</a></td></tr>
+      <tr><td>T + 21</td><td><a href="disc-expire.html">Offer expires...</a></td></tr>
+    </tbody>
+  </table>
+  <h1>Try</h1>
+  <p><strong>30 day trial</strong>The customer logged into the Portal and enabled one or more licenses</p>
+  <table>
+    <tbody>
+      <tr><td>T</td><td><a href="try-intro.html">Congrats! Here how it works</a></td></tr>
+      <tr><td>T + 14</td><td><a href="try-expire.html">Trial expiring soon</a></td></tr>
+      <tr><td>NA/A</td><td><a href="try-use.html">SPM update email with feedback solicitation</a></td></tr>
+      <tr><td>T + 37</td><td><a href="try-feedback.html">Why haven't you purchased?</a></td></tr>
+    </tbody>
+  </table>
+  <h1>Buy</h1>
+  <p>Customer chose to puchase licenses during trial period</p>
+  <table>
+    <tbody>
+      <tr><td>T</td><td><a href="buy-receipt">Why did you purchase?</a></td></tr>
+      <tr><td>T</td><td><a href="buy-feedback">Why did you cancel?</a></td></tr>
+    </tbody>
+  </table>
+</body>
+
+</html>
